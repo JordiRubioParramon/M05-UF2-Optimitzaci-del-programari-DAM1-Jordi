4b59d90c26a37720d5bfbce0404b39b19d701998
Jordi <jordirpsword@gmail.com>
2022-01-21 10:01:41 +0100

    INITIAL COMMIT

--- untitled/src/com/company/HelloDAM1.java
+++ untitled/src/com/company/HelloDAM1.java
@@ -2,8 +2,9 @@ package com.company;
 
 import java.util.Scanner;
 
-public class testgit {
+public class HelloDAM1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Crema catalana");
     }
 }
