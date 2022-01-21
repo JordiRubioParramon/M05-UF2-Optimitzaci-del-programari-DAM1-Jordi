284fb021f6d3ab09c30c0491f354d4982d303963
Jordi <jordirpsword@gmail.com>
2022-01-21 10:21:06 +0100

    INITIAL COMMIT

--- untitled/src/com/company/HelloDAM1.java
+++ untitled/src/com/company/HelloDAM1.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class HelloDAM1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("test");
     }
 }
