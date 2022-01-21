6e2ba55c236ea23628763b7aae34d04c9284d21b
Jordi <jordirpsword@gmail.com>
2022-01-21 10:29:57 +0100

    COMMIT4 Jordi

--- untitled/src/com/company/HelloDAM1.java
+++ untitled/src/com/company/HelloDAM1.java
@@ -6,5 +6,6 @@ public class HelloDAM1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Jordi Rubió");
+        System.out.println("Commit 2 Jordi");
     }
 }
